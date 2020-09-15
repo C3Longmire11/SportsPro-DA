4fc8068783f1a9b86928734c936d1da1daa8570b
Conor Longmire <55465929+C3Longmire11@users.noreply.github.com>
2020-09-15 16:31:20 -0500

    Add files via upload

---
+++ TechSupportDBConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsProDataAccessClassLibrary
+{
+    public static class TechSupportDBConnection
+    {
+        public static SqlConnection GetTechSupportDBConnection()
+        {
+            SqlConnection dataBaseConnection = new SqlConnection();
+            dataBaseConnection.ConnectionString = "Data Source = CISSQL.MATRIX.TXSTATE.EDU\\CIS3325;" +
+                " Initial Catalog = TechSupport; " +
+                "Integrated Security = true";
+            return dataBaseConnection;
+        }
+    }
+}
